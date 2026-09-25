f15b25c35ef070171bce58777dc0953d561aa105
Ali Arkan <ali.arkan4772@gmail.com>
2026-09-25 15:51:33 +0330

    Update PlayerActivity.kt

--- app/src/main/java/alzwded/openaudiobookify/PlayerActivity.kt
+++ app/src/main/java/alzwded/openaudiobookify/PlayerActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Button
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -24,6 +23,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
